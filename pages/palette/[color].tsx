0ae86385f8ee0fb71576b4004754bee7132423ce
Ben Durrant <ben.durrant@actual-experience.com>
2022-06-10 17:58:10 +0100

    Add palette cards to palette page.

--- pages/palette/[color].tsx
+++ pages/palette/[color].tsx
@@ -8,7 +8,10 @@ import {
 import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { paletteNames } from "../../src/palette";
+import palette, { Palette, paletteNames } from "../../src/palette";
+import styles from "../../styles/palette-page.module.scss";
+import { CSSProperties, useMemo } from "react";
+import { PaletteCard } from "../../src/components/palette/palette-card";
 
 export const getStaticProps: GetStaticProps = (ctx) => ({
   props: { color: ctx.params?.color },
@@ -23,22 +26,36 @@ export type PaletteDisplayProps = {
   color: string;
 };
 
-export const PaletteDisplay = ({ color }: PaletteDisplayProps) => (
-  <div>
-    <Head>
-      <title>PalettePal: {paletteNames[color]}</title>
-    </Head>
-    <TopAppBar className={`${color}-500`}>
-      <TopAppBarRow>
-        <TopAppBarSection alignStart>
-          <Link href="/">
-            <TopAppBarNavigationIcon icon="arrow_back" />
-          </Link>
-          <TopAppBarTitle>PalettePal: {paletteNames[color]}</TopAppBarTitle>
-        </TopAppBarSection>
-      </TopAppBarRow>
-    </TopAppBar>
-  </div>
-);
+export const PaletteDisplay = ({ color }: PaletteDisplayProps) => {
+  const shades = useMemo(
+    () => Object.keys(palette[color] ?? {}) as (keyof Palette)[],
+    [color]
+  );
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>PalettePal: {paletteNames[color]}</title>
+      </Head>
+      <TopAppBar className={`${styles["app-bar"]} ${color}-500`}>
+        <TopAppBarRow>
+          <TopAppBarSection alignStart>
+            <Link href="/">
+              <TopAppBarNavigationIcon icon="arrow_back" />
+            </Link>
+            <TopAppBarTitle>PalettePal: {paletteNames[color]}</TopAppBarTitle>
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
+      <div
+        className={styles["palette-grid"]}
+        style={{ "--grid-rows": shades.length } as CSSProperties}
+      >
+        {shades.map((shade) => (
+          <PaletteCard key={shade} horizontal {...{ color, shade }} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default PaletteDisplay;
--- src/components/palette/palette-card.tsx
+++ src/components/palette/palette-card.tsx
@@ -1,4 +1,3 @@
-import { Ripple } from "@rmwc/ripple";
 import { Typography } from "@rmwc/typography";
 import Link from "next/link";
 import { Palette, palette, paletteNames } from "../../palette";
@@ -8,29 +7,37 @@ export type PaletteCardProps = {
   color: string;
   shade?: keyof Palette;
   showPalette?: boolean;
+  horizontal?: boolean;
 };
 
 export const PaletteCard = ({
   color,
   shade,
   showPalette,
+  horizontal,
 }: PaletteCardProps) => (
   <Link href={`/palette/${color}`}>
-    <Ripple>
-      <div className={`${styles["palette-card"]} ${color}-${shade ?? 500}`}>
-        <div className={styles["text-row"]}>
-          <Typography use="overline" className={styles.overline}>
-            {palette[color][shade ?? 500]}
+    <div className={`${styles["palette-card"]} ${color}-${shade ?? 500}`}>
+      <div
+        className={`${styles["text-row"]} ${
+          horizontal ? styles.horizontal : ""
+        }`}
+      >
+        {shade && (
+          <Typography use="subtitle1" className={styles.title}>
+            {`${shade}`.toUpperCase()}
           </Typography>
+        )}
+        <Typography use="overline" className={styles.overline}>
+          {palette[color][shade ?? 500]}
+        </Typography>
+        {!shade && (
           <Typography use="headline6" className={styles.title}>
             {paletteNames[color]}
           </Typography>
-          {shade && (
-            <Typography use="subtitle1" className={styles.subtitle}>
-              {`${shade}`.toUpperCase()}
-            </Typography>
-          )}
-        </div>
+        )}
+      </div>
+      {!shade && (
         <div
           className={`${styles["palette-row"]} ${
             showPalette ? styles.visible : ""
@@ -47,7 +54,7 @@ export const PaletteCard = ({
             </div>
           ))}
         </div>
-      </div>
-    </Ripple>
+      )}
+    </div>
   </Link>
 );
