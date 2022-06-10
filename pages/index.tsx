f62c6993b4aad9f524b7681a5b4fc47b544e7f56
Ben Durrant <ben.durrant@actual-experience.com>
2022-06-10 13:17:41 +0100

    Add app bar.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,39 @@
+import {
+  TopAppBar,
+  TopAppBarFixedAdjust,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarTitle,
+} from "@rmwc/top-app-bar";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Fragment } from "react";
 import { PaletteCard } from "../src/components/palette/palette-card";
 import { WBPaletteCard } from "../src/components/palette/wb-palette-card";
-import palette from "../src/palette";
+import palette, { paletteNames } from "../src/palette";
 import styles from "../styles/home.module.scss";
-
-console.log(palette);
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>PalettePal</title>
         <meta name="description" content="Material Design Palette" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <TopAppBar className={styles["app-bar"]}>
+        <TopAppBarRow>
+          <TopAppBarSection alignStart>
+            <TopAppBarTitle>PalettePal</TopAppBarTitle>
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
       <div className={styles["palette-grid"]}>
         {Object.keys(palette).map((color, index) => (
-          <>
+          <Fragment key={color}>
             {index === 2 && <WBPaletteCard />}
-            <PaletteCard key={color} color={color} shade={500} />
-          </>
+            <PaletteCard color={color} shade={500} />
+          </Fragment>
         ))}
       </div>
     </div>
